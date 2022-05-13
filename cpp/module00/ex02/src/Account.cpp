decfef3cd5d9db4959da4a232d9b31eda6aa8296
pouet <jm@Tfes>
2022-05-13 18:45:29 +0200

    push

---
+++ cpp/module00/ex02/src/Account.cpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: njaros <njaros@student.42lyon.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/13 18:39:58 by njaros            #+#    #+#             */
+/*   Updated: 2022/05/13 18:40:55 by njaros           ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+
+void	makeDeposit( int deposit );
+{
+
+}
+
+bool	makeWithdrawal( int withdrawal );
+{
+
+}
+
+int		checkAmount( void ) const;
+{
+
+}
+
+void	displayStatus( void ) const;
+{
+	
+}
